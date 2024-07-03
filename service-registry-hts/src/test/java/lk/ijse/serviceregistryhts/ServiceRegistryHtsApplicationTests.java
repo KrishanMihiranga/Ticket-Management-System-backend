8edde1043f6536feb27cd408962e58ea4f7867dc
KrishanMihiranga <xp3cto95@gmail.com>
2024-07-03 13:36:33 +0530

    feat: service registry

---
+++ service-registry-hts/src/test/java/lk/ijse/serviceregistryhts/ServiceRegistryHtsApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.serviceregistryhts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryHtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
